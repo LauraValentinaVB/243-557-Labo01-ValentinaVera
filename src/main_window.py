465dd13a1fea20bf7a647e5f9de5ee6b5821e91e
Valentina Vera <2448409@cegeplimoilou.ca>
2026-08-24 16:30:31 -0400

    Ajout de la classe Sensor et intégration à PyQt

--- src/main_window.py
+++ src/main_window.py
@@ -4,6 +4,7 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget,
 )
+from sensor import Sensor
 
 
 class MainWindow(QWidget):
@@ -15,8 +16,15 @@ class MainWindow(QWidget):
         self.setWindowTitle("243-557 — DiagnosticTool")
         self.resize(360, 220)
 
+        self.sensor = Sensor(
+            "Distance",
+            "cm",
+            35.0,
+        )
+
         self.title_label = QLabel("Logiciel de diagnostic - Valentina Vera")
-        self.sensor_name_label = QLabel("Capteur : Température")
+        self.sensor_name_label = QLabel ( f"Nom : {self.sensor.name}")
+        self.sensor_unit_label = QLabel(f"Unité : {self.sensor.unit}")
         self.sensor_value_label = QLabel("Valeur : ---")
         self.read_button = QPushButton("Lire le capteur")
 
@@ -24,6 +32,7 @@ class MainWindow(QWidget):
         layout.addWidget(self.title_label)
         layout.addWidget(self.sensor_name_label)
         layout.addWidget(self.sensor_value_label)
+        layout.addWidget(self.sensor_unit_label)
         layout.addWidget(self.read_button)
 
         self.setLayout(layout)
@@ -31,5 +40,8 @@ class MainWindow(QWidget):
         self.read_button.clicked.connect(self.read_sensor)
 
     def read_sensor(self) -> None:
-        """Simule la lecture d'un capteur de température."""
-        self.sensor_value_label.setText("Valeur : 22.5 °C")
+        value = self.sensor.read()
+
+        self.sensor_value_label.setText(
+            f"Valeur : {value}"
+        )
